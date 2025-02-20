5499cdb5fc5b055e3803ac7215d3a59ab7d0c256
xyzlearns <asmitjyotib@gmail.com>
2025-02-20 21:02:57 +0530

    deploying

--- ui.py
+++ ui.py
@@ -6,7 +6,7 @@ st.title("💰 AI Financial Advisor")
 expense = st.number_input("Enter your monthly expense:", min_value=0)
 
 if st.button("Predict Spending Category"):
-    url = "https://ai-financial-advisor-tcp8.onrender.com/predict"
+    url = "https://ai-financial-advisor-1.onrender.com/predict"
     data = {"expense": expense}
     response = requests.post(url, json=data)
 
