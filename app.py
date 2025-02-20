f90e2268908906dd56c1e78db1b54c1e7755592e
Asmitjyoti Barman <asmitjyotib@gmail.com>
2025-02-20 21:27:06 +0530

    Update app.py
    fixed

--- app.py
+++ app.py
@@ -14,4 +14,7 @@ def predict():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    import os
+port = int(os.environ.get("PORT", 8080))  # Use PORT from environment or default to 8080
+serve(app, host="0.0.0.0", port=port)
+
